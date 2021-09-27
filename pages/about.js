f407f1f27501a9650d57569a4292423e6d00938a
claynaut <jpesc002@ucr.edu>
2021-09-27 02:33:27 -0700

    Placeholder text

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,33 @@
 export default function About() {
   return (
     <section className="flex w-full max-w-5xl h-screen items-center">
-      <h1 className="font-semibold text-5xl">
-        About Us
-      </h1>
+      <div>
+        <h1 className="font-semibold text-5xl">
+          About Us
+        </h1>
+        <h2 className="my-4 font-semibold text-3xl">
+          What is BioHack?
+        </h2>
+        <p className="my-4 text-lg">
+          BioHack is a 24-hour bioengineering-focused, health and medicine-themed,
+          hackathon at the University of California, Riverside.
+        </p>
+        <h2 className="my-4 font-semibold text-3xl">
+          Our Mission
+        </h2>
+        <p className="my-4 text-lg">
+          Our mission is to inspire new hackers to collaborate with students and
+          sponsors throughout computer science and bioengineering. We encourage
+          students of any background and in any field of study to participate in our
+          inclusive event. We also motivate our participants to foster strong,
+          professional relationships with industrial and academic sponsors.
+        </p>
+        <p className="my-4 text-lg">
+          By hosting BioHack within the Inland Empire, our goal is to have sponsors
+          and students foster connections with engineers throughout the bioengineering
+          and computer science community across California.
+        </p>
+      </div>
     </section>
   )
 }
--- pages/sponsors.js
+++ pages/sponsors.js
@@ -1,9 +1,14 @@
 export default function Sponsors() {
   return (
     <section className="flex w-full max-w-5xl h-screen items-center">
-      <h1 className="font-semibold text-5xl">
-        Sponsors
-      </h1>
+      <div>
+        <h1 className="font-semibold text-5xl">
+          Sponsors
+        </h1>
+        <p className="my-4 text-lg">
+          A huge thank you to all of our sponsors who made this hackathon possible!
+        </p>
+      </div>
     </section>
   )
 }
