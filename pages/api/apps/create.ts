2bc3f76600f7dfdb1fd7e1af1ea44a4da7ae2577
claynaut <jpesc002@ucr.edu>
2021-11-18 01:14:08 -0800

    Implement API routes

--- pages/api/apps/create.ts
+++ pages/api/apps/create.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { connectToDatabase } from '../../../util/mongodb'
-import { getSession } from 'next-auth/client'
+import clientPromise from '@/lib/mongodb'
+import { getSession } from 'next-auth/react'
+import { nanoid } from 'nanoid'
 
-export default async function CreateCheckIn(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getSession({ req });
+export default async function createApp(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req })
+  const db = (await clientPromise).db(process.env.MONGODB_DB)
   if (session) {
-    const { db } = await connectToDatabase();
     const {
       user: [ 
         first_name,
@@ -18,7 +19,6 @@ export default async function CreateCheckIn(req: NextApiRequest, res: NextApiRes
         major,
         graduation_date,
         graduated,
-        resume,
         first_time,
         github,
         linkedin,
@@ -27,41 +27,47 @@ export default async function CreateCheckIn(req: NextApiRequest, res: NextApiRes
         additional_info,
         goal,
         source,
-        tool_experience,
-        id 
+        tool_experience
       ]
-    } = req.body;
+    } = req.body
     
-    await db.collection('checkins').insertOne({
-      first_name: first_name,
-      last_name: last_name,
-      phone_number: phone_number,
-      gender: gender,
-      ethnicity: ethnicity,
-      school: school,
-      year: year,
-      major: major,
-      graduation_date: graduation_date,
-      graduated: graduated,
-      first_time: first_time,
-      resume: resume,
-      github: github,
-      linkedin: linkedin,
-      portfolio: portfolio,
-      project_story: project_story,
-      additional_info: additional_info,
-      goal: goal,
-      source: source,
-      tool_experience: tool_experience,
-      userId: id,
-      qualified: '',
-      groupId: '',
-      createdAt: new Date()
-    });
+    await db.collection('users').updateOne(
+      {
+        email: session.user.email
+      },
+      {
+        $set: {
+          first_name: first_name,
+          last_name: last_name,
+          phone_number: phone_number,
+          gender: gender,
+          ethnicity: ethnicity,
+          school: school,
+          year: year,
+          major: major,
+          graduation_date: graduation_date,
+          graduated: graduated,
+          first_time: first_time,
+          github: github,
+          linkedin: linkedin,
+          portfolio: portfolio,
+          project_story: project_story,
+          additional_info: additional_info,
+          goal: goal,
+          source: source,
+          tool_experience: tool_experience,
+          uid: nanoid(),
+          qualified: '',
+          gid: '',
+          admin: false,
+          createdAt: new Date()
+        }
+      }
+    )
   
-    res.status(200).end();
+    res.status(200).json({})
   }
   else {
-    res.status(401).end();
+    res.status(401).json({})
   }
 }
--- pages/api/groups/all.ts
+++ pages/api/groups/all.ts
@@ -2,14 +2,14 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import clientPromise from '@/lib/mongodb'
 import { getSession } from 'next-auth/react'
 
-export default async function queryApps(req: NextApiRequest, res: NextApiResponse) {
+export default async function queryGroups(req: NextApiRequest, res: NextApiResponse) {
   const session = await getSession({ req })
   const db = (await clientPromise).db(process.env.MONGODB_DB)
-
-  if (session && session.user.admin) {
-    const users = await db.collection('users').find().toArray()
-
-    res.status(200).json({ users })
+  
+  if (session && session.user.qualified === 'yeah' && session.user.gid !== '') {
+    const groups = await db.collection('groups').find().toArray()
+    
+    res.status(200).json({ groups })
   }
   else {
     res.status(401).json({})
