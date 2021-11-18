be47af185a2c04f0ec47f6fb24251c5e89147ccc
claynaut <jpesc002@ucr.edu>
2021-11-18 15:56:33 -0800

    Fix page title check

--- components/Page.tsx
+++ components/Page.tsx
@@ -4,7 +4,7 @@ export default function Page({ title = '', children }) {
   return (
     <main className='flex flex-col justify-center items-center px-4 w-full min-h-screen'>
       <Head>
-        <title>BioHack 2022 { title !== '' && ('| ' + title) }</title>
+        <title>BioHack 2022 { title && title !== '' && ('| ' + title) }</title>
       </Head>
       <div className='w-full max-w-[60rem]'>
         <section className='flex flex-col w-full'>
