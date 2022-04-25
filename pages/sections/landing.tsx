5b8f48bed414617e6b8b6153997961f49e53555e
claynaut <hello@jspescas.io>
2022-04-25 16:20:29 -0700

    Fix sizing

--- pages/sections/landing.tsx
+++ pages/sections/landing.tsx
@@ -1,7 +1,7 @@
 export default function Landing() {
   return (
     <section className='flex w-full max-w-[64rem] h-screen min-h-[48rem] md:min-h-[60rem] my-12 lg:mt-0 items-center justify-center xs:justify-start'>
-      <div className='text-center xs:text-left'>
+      <div className='w-full text-center xs:text-left'>
         <h1 className='text-6xl md:text-7xl font-semibold'>
           BIO<b>HACK</b>
         </h1>
