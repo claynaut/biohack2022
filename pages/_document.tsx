669004324a88b14b5d43c30c20d71a7d2de61e1e
claynaut <hello@jspescas.io>
2022-04-26 03:03:27 -0700

    Update meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en' className='m-0 p-0 box-border overflow-x-hidden font-sans'>
         <Head>
-          <meta name='description' content='BioHack, a bioengineering-focused, health and medicine-themed, hackathon at the University of California, Riverside.' />
+          <meta name='description' content='BioHack, a 24-hour healthcare hackathon hosted at University of California, Riverside.' />
           <link href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap' rel='stylesheet' />
           <link rel='preload' as='image' href='/assets/sponsors/aspb.png' />
           <link rel='preload' as='image' href='/assets/sponsors/kgi.png' />
